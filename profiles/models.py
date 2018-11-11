0eabe12f196d42cdb9ee89ad50b48c2724bedde8
april7229 <martinezapril56.com>
2018-11-11 01:41:49 +0000

    lol

--- profiles/models.py
+++ profiles/models.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.db import models
 from allauth.account.signals import user_logged_in, user_signed_up
-
+import stripe
 # Create your models here
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 class profile(models.Model):
     name = models.CharField(max_length=120)
@@ -24,28 +26,23 @@ class userStripe(models.Model):
         else:
             return self.user.username
 
-def my_callback(sender, request, user, **kwargs):
-    idStripe, created = userStripe.objects.get_or_create(user=user)
+def stripeCallback(sender, request, user, **kwargs):
+    user_stripe_account, created = userStripe.objects.get_or_create(user=user)
     if created:
-        print 'created for %s'%(user.username)
+        print 'created for %s' %(user.username)
+    if user_stripe_account.stripe_id is None or user_stripe_account.stripe_id == '':
+        new_stripe_id = stripe.Customer.create(email=user.email)
+        user_stripe_account.stripe_id = new_stripe_id['id']
+        user_stripe_account.save()
 
+def profileCallback(sender, request, user, **kwargs):
     userProfile, is_created = profile.objects.get_or_create(user=user)
     if is_created:
         userProfile.name = user.username
         userProfile.save()
 
-def stripeCallback(sender, request, user, **kwargs):
-    idStripe, created = userStripe.objects.get_or_create(user=user)
-    if created:
-        print 'created for %s'%(user.username)
-
-def profileCallback(sender, request, user, **kwargs):
-    userProfile, is_created = profile.objects.get_or_create(user=user)
-    if is_create:
-        userProfile.name = user.username
-        userProfile.save()
-
 
 user_logged_in.connect(stripeCallback)
-user_logged_in.connect(profileCallback)
+user_signed_up.connect(stripeCallback)
+user_signed_up.connect(profileCallback)
 
